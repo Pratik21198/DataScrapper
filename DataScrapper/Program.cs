47cd5dae1c2dbc2fdceb03bfc2b32569e886b748
Pratik21198 <35757191+Pratik21198@users.noreply.github.com>
2021-09-03 13:00:39 +0530

    updates accirdubg to site structure update

--- DataScrapper/Program.cs
+++ DataScrapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -7,43 +8,43 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace DataScrapper
 {
   class Program
   {
     private static string sqlConnectionString = ConfigurationManager.AppSettings["SqlConnectionString"];
-    static int pageNo = 1;
+    static bool ResetPage = false;
     static void Main(string[] args)
     {
       try
       {
+        DateTime startTime, endTime;
+        startTime = DateTime.Now;
+
         DataTable dtCategories = GetCategories();
         int cat = 0;
+        int pageNo = 1;
         while (cat < dtCategories.Rows.Count)
         {
-          try
-          {
-            string Category = Convert.ToString(dtCategories.Rows[cat]["Category"]);
-            string ListUrlBase = string.Format("{0}{1}",
-                                                Convert.ToString(dtCategories.Rows[cat]["CategoryUrl"]),
-                                                pageNo > 1 ? string.Format("?page={0}", pageNo) : "");
-            Console.WriteLine(string.Format("Importing category: {0}", Category));
-            Console.WriteLine("");
-            ImportData(Category, ListUrlBase, pageNo);
+          string Category = Convert.ToString(dtCategories.Rows[cat]["Category"]);
+          string ListUrlBase = string.Format("{0}{1}",
+                                              Convert.ToString(dtCategories.Rows[cat]["CategoryUrl"]),
+                                              pageNo > 1 ? string.Format("?page={0}", pageNo) : "");
+          Console.WriteLine(string.Format("Importing category: {0}", Category));
+          Console.WriteLine("");
+          ImportData(Category, ListUrlBase, pageNo);
 
-            //move to next category
-            pageNo = 1;
-            cat++;
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine("Retrying..");
-            Console.WriteLine("");
-            RemovePageData(Convert.ToString(dtCategories.Rows[cat]["Category"]));
-          }
+          //move to next category
+          pageNo = 1;
+          cat++;
         }
 
+        endTime = DateTime.Now;
+        Console.WriteLine(string.Format("Started: {0}", startTime.ToString()));
+        Console.WriteLine(string.Format("Finished: {0}", endTime.ToString()));
+        Console.ReadKey();
       }
       catch (Exception ex)
       {
@@ -85,12 +86,18 @@ namespace DataScrapper
 
             Task.WaitAll(lstTask.ToArray());
           }
-          ListUrlBase = ListUrlBase.Replace(string.Format("?page={0}", pageNo), "");
-          pageNo++;
-          ListUrl = string.Format("{0}?page={1}", ListUrlBase, pageNo);
+          if (ResetPage)
+          {
+            RemovePageData(Category, pageNo);
+            ResetPage = false;
+          }
+          else
+          {
+            ListUrlBase = ListUrlBase.Replace(string.Format("?page={0}", pageNo), "");
+            pageNo++;
+            ListUrl = string.Format("{0}?page={1}", ListUrlBase, pageNo);
+          }       
         }
-        Console.WriteLine("\nFINISHED");
-        Console.ReadKey();
       }
       catch (Exception ex)
       {
@@ -103,52 +110,72 @@ namespace DataScrapper
       {
         string BaseUrl = "https://meesho.com";
         string productUrl = string.Format("{0}{1}",
-                                                 BaseUrl,
-                                                 product.ChildNodes[0].Attributes["href"].Value);
+                                           BaseUrl,
+                                           product.ChildNodes[0].ChildNodes[0].Attributes["href"].Value);
 
         HtmlWeb web = new HtmlWeb();
         HtmlDocument productPage = web.Load(productUrl);
 
         string imageUrl = productPage.DocumentNode
-                                     .SelectSingleNode("//section[@class='image-wrapper']")
-                                     .ChildNodes[0].Attributes["data-src"].Value;
+                                     .SelectSingleNode("//section[@class='ProductDisplay__ImageWrapper-sc-2h14i1-0 hIkCDs']")
+                                     .ChildNodes[0].Attributes["src"].Value;
 
         string removeBaseImageURL = imageUrl.Replace("https://images.meesho.com/images/products/", "");
 
         string sku = string.Format("S-{0}", removeBaseImageURL.Substring(0, removeBaseImageURL.IndexOf('/')));
 
-        using(WebClient imageClient = new WebClient())
+        using (WebClient imageClient = new WebClient())
         {
-          imageClient.DownloadFile(imageUrl, string.Format(@"E:\_Storefuse\IMG\Products\{0}.jpg", sku));
+          Directory.CreateDirectory(string.Format(@"E:\_Storefuse\IMG\Products\{0}", Category.Replace(" > ", "\\")));
+          imageClient.DownloadFile(imageUrl, string.Format(@"E:\_Storefuse\IMG\Products\{0}\{1}.jpg", Category.Replace(" > ", "\\"), sku));
         }
 
         string productName = productPage.DocumentNode
-                                        .SelectSingleNode("//h1[@class='pdp-title']")
+                                        .SelectSingleNode("//h1[@class='ShippingInfo__PdpTitle-sc-17v6v24-1 dHcaWM']")
                                         .InnerText.Replace("Trusted", "");
 
         string price = productPage.DocumentNode
-                                  .SelectSingleNode("//div[@class='actual-cost']")
+                                  .SelectSingleNode("//div[@class='ShippingInfo__ActualCost-sc-17v6v24-3 izQIMN']")
                                   .InnerText.Substring(1);
+        string mrp = "";
 
-        string dispatchETA = productPage.DocumentNode
-                                        .SelectSingleNode("//ul[@class='info-product']")
-                                        .ChildNodes[0].ChildNodes[1].InnerText;
+        HtmlNode maxRetailPrice = productPage.DocumentNode
+                                             .SelectSingleNode("//div[@class='ShippingInfo__MrpCost-sc-17v6v24-8 hMzqvR']");
+        if (maxRetailPrice != null && maxRetailPrice.ChildNodes.Count > 0)
+        {
+          mrp = maxRetailPrice.ChildNodes[0].InnerHtml.Trim().Substring(14);
+        }
 
+        string deliveryCharge = "";
+        string dispatchETA = "";
 
-        HtmlNode sizeList = productPage.DocumentNode
-                                       .SelectSingleNode("//div[@class='chip-list']");
+        HtmlNode shippingInfo = productPage.DocumentNode
+                                           .SelectSingleNode("//ul[@class='ShippingInfo__InfoProducts-sc-17v6v24-9 jtJOHC']");
+
+        if (shippingInfo.ChildNodes.Count > 1)
+        {
+          deliveryCharge = shippingInfo.ChildNodes[0].ChildNodes[2].InnerText.Trim().Substring(1);
+          dispatchETA = shippingInfo.ChildNodes[1].ChildNodes[1].InnerText;
+        }
+        else
+        {
+          dispatchETA = shippingInfo.ChildNodes[0].ChildNodes[1].InnerText;
+        }
+
+        HtmlNodeCollection sizeList = productPage.DocumentNode
+                                       .SelectNodes("//div[@class='Prod_Avail_Info__ChipButton-sc-vkc520-3 jZZvYO']");
         string size = "";
         if (sizeList != null)
-        { size = string.Join(" | ", sizeList.ChildNodes.Select(i => i.InnerText)); }
+        { size = string.Join(" | ", sizeList.Select(i => i.InnerText)); }
 
 
         string description = string.Join(" | ", productPage.DocumentNode
-                                                          .SelectSingleNode("//section[@class='product-description']")
+                                                          .SelectSingleNode("//section[@class='ProductDescription__ProductDescriptionWrapper-sc-1rx3r0k-0 jmHdcm']")
                                                           .ChildNodes[1].ChildNodes.Select(i => string.Format("{0}{1}",
                                                                                                               i.ChildNodes[0].InnerText,
                                                                                                               i.ChildNodes[1].InnerText)));
         string soldBy = productPage.DocumentNode
-                                   .SelectSingleNode("//div[@class='sold-by']")
+                                   .SelectSingleNode("//div[@class='SupplierSection__SoldBy-sc-m2zjrn-3 djjTGt']")
                                    .InnerText;
 
         string sqlQuery = @"INSERT INTO [dbo].[MeeshoProducts]
@@ -163,6 +190,8 @@ namespace DataScrapper
                                    ,[SoldBy]
                                    ,[ImageUrl]
                                    ,[Price]
+                                   ,[DeliveryCharge]
+                                   ,[MRP]
                                  )
                                  VALUES(
                                     @SKU
@@ -175,6 +204,8 @@ namespace DataScrapper
                                    ,@SoldBy
                                    ,@ImageUrl
                                    ,@Price
+                                   ,@DeliveryCharge
+                                   ,@MRP
                                  )";
 
         using (SqlConnection cnn = new SqlConnection(sqlConnectionString))
@@ -191,6 +222,8 @@ namespace DataScrapper
             cmd.Parameters.AddWithValue("@SoldBy", soldBy);
             cmd.Parameters.AddWithValue("@ImageUrl", imageUrl);
             cmd.Parameters.AddWithValue("@Price", price);
+            cmd.Parameters.AddWithValue("@DeliveryCharge", string.IsNullOrWhiteSpace(deliveryCharge) ? "0" : deliveryCharge);
+            cmd.Parameters.AddWithValue("@MRP", string.IsNullOrWhiteSpace(mrp) ? "0" : mrp);
 
             cnn.Open();
             cmd.ExecuteNonQuery();
@@ -200,7 +233,7 @@ namespace DataScrapper
       }
       catch (Exception ex)
       {
-        throw ex;
+        ResetPage = true;
       }
     }
     static DataTable GetCategories()
@@ -224,13 +257,13 @@ namespace DataScrapper
       { throw ex; }
       return dtCategories;
     }
-    static void RemovePageData(string category)
+    static void RemovePageData(string category, int pageNo)
     {
       try
       {
         using (SqlConnection cnn = new SqlConnection(sqlConnectionString))
         {
-          using (SqlCommand cmd = new SqlCommand(string.Format("DELETE MeeshoProducts WHERE Category = '{0}' AND Page = {1}", category, pageNo), 
+          using (SqlCommand cmd = new SqlCommand(string.Format("DELETE MeeshoProducts WHERE Category = '{0}' AND Page = {1}", category, pageNo),
                                                 cnn))
           {
             cnn.Open();
